42e84a7428ef41d0956a4d74b40525e3c257b037
William Nayar <117164313+Wnayar@users.noreply.github.com>
2026-07-29 00:58:04 +0800

    Add: greatest-common-divisor-of-strings - submission-2

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len1 = len(str1)
+        len2 = len(str2)
+        minLen = min(len1, len2)
+        minstr = None
+
+        if minLen == len1:
+            minstr = str1
+        else:
+            minstr = str2
+
+        def isDivisible(l: int) -> bool:
+            # if not multiples return 
+            if len1 % l != 0 or len2 % l != 0:
+                return False 
+            
+            # if multiples then check if valid 
+            f1 = len1 // l
+            f2 = len2 // l
+            str1New = f1 * minstr[:l]
+            str2New = f2 * minstr[:l]
+            if str1New != str1 or str2New != str2:
+                return False 
+            return True 
+
+        for i in range(minLen, 0, -1):
+            if isDivisible(i):
+                return minstr[:i]
+        
+        return ""
+
+
+
+        
