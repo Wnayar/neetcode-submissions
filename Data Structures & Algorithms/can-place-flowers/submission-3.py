59e6440e5178d11849b1beba673c2d84e0f26fb1
William Nayar <117164313+Wnayar@users.noreply.github.com>
2026-07-31 22:24:12 +0800

    Add: can-place-flowers - submission-3

---
+++ Data Structures & Algorithms/can-place-flowers/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    # time compelxity O(n), space complexity O(1)
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for i in range(len(flowerbed)):
+            if i == 0:
+                if flowerbed[i] == 0:
+                    if i + 1 == len(flowerbed) or flowerbed[i + 1] == 0:
+                        flowerbed[i] = 1
+                        n -= 1
+                        continue 
+            elif i == len(flowerbed) - 1:
+                if flowerbed[i] == 0:
+                    if i == 0 or flowerbed[i - 1] == 0:
+                        flowerbed[i] = 1
+                        n -= 1
+                        continue     
+                continue
+            elif flowerbed[i - 1] == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+
+        return n <= 0
