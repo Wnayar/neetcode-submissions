e35301b4ee09382c3ca42c6b1d1c21ada35fd45a
William Nayar <117164313+Wnayar@users.noreply.github.com>
2026-08-05 19:52:11 +0800

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    # time complexity: O(n)
+    # space complexity: O(h), where h is height of the binary tree (the longest recursive stack)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node, l , r):
+            # base case 
+            if node is None:
+                return True 
+            
+            # recursion checking valid range 
+            if node.val > l and node.val < r:
+                return dfs(node.left, l, node.val) and dfs(node.right, node.val, r)
+            else:
+                return False  
+        
+        return dfs(root, float('-inf') , float('inf'))
